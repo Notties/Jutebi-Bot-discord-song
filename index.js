d8e65311e2430868e6a5076843ed65ab4d3fc16d
Knot <akthakorn@gmail.com>
2022-06-06 15:52:23 +0700

    update status bot

--- index.js
+++ index.js
@@ -55,6 +55,20 @@ if (LOAD_SLASH) {
 else {
     client.on("ready", () => {
         console.log(`Logged in as ${client.user.tag}`)
+
+        const arrayOfStatus = [
+            `Over ${client.GUILDS.cache.size} Servers!`
+            `Prefix is: '/'`
+            `Jutebi Jutebi ❤️`
+        ];
+        
+        let index = 0;
+        setInterval(() => {
+            if(index === arrayOfStatus.length) index = 0;
+            const status = arrayOfStatus[index];
+            client.user.setActivity(status, {type: "WATCHING"}).catch(console.error)
+            index++;
+        }, 5000)
     })
     client.on("interactionCreate", (interaction) => {
         async function handleCommand() {
